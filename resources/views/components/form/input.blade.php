cfd6ce20c9b57c116f004d81586f43559dd92678
FCNO <fcno.dev@gmail.com>
2022-03-16 21:37:10 -0300

    chore(view): cria componente anônimo input

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,65 @@
+{{--
+    Input padrão.
+
+    @see https://laravel.com/docs/9.x/blade
+    @see https://tailwindcss.com/
+    @see https://tailwindcss.com/docs/dark-mode
+    @see https://laravel-livewire.com
+    @see https://alpinejs.dev/
+--}}
+
+
+@props(['error' => false, 'icon', 'id', 'text', 'title'])
+
+
+{{-- container do input --}}
+<div class="text-left w-full" title="{{ $title }}">
+
+    {{-- texto acima do input --}}
+    <label class="font-bold text-lg" for="{{ $id }}">
+
+        {{ $text }}
+
+
+        @if ($attributes->has('required'))
+
+            <span class="text-red-500">*</span>
+
+        @endif
+
+    </label>
+
+
+    <div class="bg-primary-100 border-2 border-primary-300 flex items-center rounded">
+
+        {{-- ícone à frente do input --}}
+        <label class="text-primary-900 p-2" for="{{ $id }}">
+
+            <x-icon name="{{ $icon }}"/>
+
+        </label>
+
+
+        {{-- input propriamente dito --}}
+        <input
+            id="{{ $id }}"
+            name="{{ $id }}"
+            {{
+                $attributes
+                ->merge(['class' => 'p-2 text-primary-900 truncate w-full focus:outline-primary-500'])
+                ->when($error, function ($collection) {
+                    return $collection->merge(['class' => 'invalid']);
+                })
+            }}
+            {{ $attributes->except('class') }}/>
+
+    </div>
+
+
+    @if ($error)
+
+        <p class="text-red-500 text-right text-sm">{{ $error }}</p>
+
+    @endif
+
+</div>
