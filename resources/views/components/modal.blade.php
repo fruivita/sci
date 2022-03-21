92d29a23104dfe881f1e116552188ea58532b54c
FCNO <fcno.dev@gmail.com>
2022-03-21 05:01:33 -0300

    feat(view): aplicar opacidade ao modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -12,15 +12,20 @@
 <div
     x-data="{ open: @entangle('show_edit_modal').defer }"
     x-show="open"
-    x-transition
-    class="fixed flex inset-0 items-center justify-center text-primary-900 z-40 dark:text-secondary-50"
+    x-transition.duration.500ms
+    class="fixed flex inset-0 items-center justify-center text-primary-900 z-30 dark:text-secondary-50"
     {{ $attributes }}
 >
 
+    {{-- modal background --}}
+    <div class="fixed inset-0 bg-primary-100 opacity-90 dark:bg-secondary-900"></div>
+
+
+    {{-- modal propriamente dito --}}
     <article
         x-on:click.away="open = false"
         x-on:keyup.escape.window="open = false"
-        class="divide-y w-full lg:w-10/12">
+        class="divide-y w-full z-40 lg:w-10/12">
 
         <header class="bg-primary-300 rounded-t-lg p-3 dark:bg-secondary-600">
 
