b93f0c2f2ef103a0b0ac1642d1aee0fee2b90af3
FCNO <fcno.dev@gmail.com>
2022-03-25 19:20:06 -0300

    chore(view): cria componente de mensagem inline
    Idealizado para ser utilizado ao lado dos botões de ação para feedback
    menos intrusivo que as caixas de mensagem.

---
+++ resources/views/components/feedback/inline.blade.php
@@ -0,0 +1,30 @@
+{{--
+    Feedback ao usuário para ser, preferencialmente, adicionado ao lado do
+    botão de ação para exibir de retorno ao usuário.
+
+    O componente aguarda a emissão do evento 'feeback' acompanhado de uma
+    string para exibição.
+
+    @see https://laravel.com/docs/9.x/blade
+    @see https://tailwindcss.com/
+    @see https://tailwindcss.com/docs/dark-mode
+    @see https://laravel-livewire.com
+    @see https://alpinejs.dev/
+--}}
+
+<span
+    x-data="{ showInlineFeedback : false , message : '' }"
+    x-init="
+        @this.on('feedback', ( e ) => {
+            setTimeout(() => {
+                showInlineFeedback = false;
+            }, 2500);
+            showInlineFeedback = true;
+            message = e;
+        })
+    "
+    x-show="showInlineFeedback"
+    x-text="message"
+    x-transition.duration.500ms
+    class="font-bold text-center"
+></span>
