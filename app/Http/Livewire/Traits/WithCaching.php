2ad8d70d3ee1b44e951768b7d97dafdf0ee97ab5
FCNO <fcno.dev@gmail.com>
2022-03-25 01:25:09 -0300

    chore(core): implementa cache por meio de trait
    Idealizado para ser utilizado no Livewire em queries consideradas
    'imutáveis', isto é, com resultados que serão iguais com razoavel
    certeza.
    
    O cache dura por apenas 1 minuto e sua lógica foi implementada por trait

---
+++ app/Http/Livewire/Traits/WithCaching.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Livewire\Traits;
+
+use Closure;
+use Illuminate\Support\Facades\Cache;
+
+/**
+ * Trait para criar cache, bem como usá-lo quando necessário
+ *
+ * @see https://www.php.net/manual/en/language.oop5.traits.php
+ * @see https://laravel-livewire.com/docs/2.x/traits
+ */
+trait WithCaching
+{
+    /**
+     * O cache deve ser utilizado?
+     *
+     * @var false
+     */
+    public $use_cache = false;
+
+    /**
+     * Define que o cache deve ser utilizado.
+     *
+     * @return void
+     */
+    public function useCache()
+    {
+        $this->use_cache = true;
+    }
+
+    /**
+     * Armazena o resultado no callback no cache com a chave informada e o
+     * retorna.
+     *
+     * Se a chave já existir no cache, o item será retornado, caso contrário
+     * o cache será criado e então retornado ao chamador.
+     *
+     * @param string $key chave do cache
+     * @param \Closure $callback
+     *
+     * @return mixed
+     */
+    public function cache(string $key, Closure $callback)
+    {
+        $key = $key . $this->id;
+
+        if ($this->use_cache && Cache::has($key)) {
+            return Cache::get($key);
+        }
+
+        $result = $callback();
+
+        Cache::put($key, $result, now()->addMinute());
+
+        return $result;
+    }
+}
