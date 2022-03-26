ce2da40cbcce810a608cf699eb38ea4a4e394d93
FCNO <fcno.dev@gmail.com>
2022-03-26 03:45:59 -0300

    test: atualiza testes do id das permissões

--- tests/Unit/App/Models/RoleTest.php
+++ tests/Unit/App/Models/RoleTest.php
@@ -74,8 +74,8 @@ test('ids dos perfis estão definidos', function () {
 });
 
 test('ids dos permissões para administração do perfil estão definidas', function () {
-    expect(Role::VIEWANY)->toBe(10000)
-    ->and(Role::UPDATE)->toBe(13000);
+    expect(Role::VIEWANY)->toBe(100001)
+    ->and(Role::UPDATE)->toBe(100003);
 });
 
 test('cadastra múltiplos perfis', function () {
