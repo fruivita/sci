4975cd0f5064880508a6255daae386a4758a1e0a
FCNO <fcno.dev@gmail.com>
2022-03-30 19:14:29 -0300

    chore(policies): cria policy para simulação de uso
    - Adiciona testes

---
+++ app/Policies/SimulationPolicy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+/**
+ * @see https://laravel.com/docs/9.x/authorization
+ */
+class SimulationPolicy extends Policy
+{
+    /**
+     * Determine whether the user can create simulations.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function create(User $user)
+    {
+        return
+            session()->missing('simulated')
+            && $this->hasPermissionWithoutCache($user, User::SIMULATION_CREATE);
+    }
+
+    /**
+     * Determine whether the user can delete the simulation.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function delete(User $user)
+    {
+        return session()->has('simulated');
+    }
+}
