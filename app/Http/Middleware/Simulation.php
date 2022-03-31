39b5efe5657d9fd2369d33e9c90277147bda9498
FCNO <fcno.dev@gmail.com>
2022-03-31 03:29:24 -0300

    chore(core): middleware de simulação por 1 request
    O middleware mantem o usuário simulado por 1 request renovável em vez de
    indefinidamente.

---
+++ app/Http/Middleware/Simulation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * @see https://laravel.com/docs/9.x/middleware
+ */
+class Simulation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (session()->has('simulated')) {
+            Auth::onceUsingID(session('simulated')->getAuthIdentifier());
+        }
+
+        return $next($request);
+    }
+}
