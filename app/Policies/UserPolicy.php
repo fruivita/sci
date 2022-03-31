0ba5af4388f3c5af5725bb2a08b12818aa54ce32
FCNO <fcno.dev@gmail.com>
2022-03-31 20:33:15 -0300

    chore(policies): policy para ações sobre o usuário

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+/**
+ * @see https://laravel.com/docs/9.x/authorization
+ */
+class UserPolicy extends Policy
+{
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function viewAny(User $user)
+    {
+        return $this->hasPermissionWithCache($user, User::VIEWANY);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function update(User $user)
+    {
+        return $this->hasPermissionWithCache($user, User::UPDATE);
+    }
+}
---
+++ tests/Feature/Policies/UserPolicyTest.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Models\User;
+use App\Policies\UserPolicy;
+use Illuminate\Support\Facades\Cache;
+
+beforeEach(function () {
+    $this->user = login('foo');
+});
+
+afterEach(function () {
+    logout();
+});
+
+// Forbidden
+test('usuário sem permissão não pode listar os usuários', function () {
+    expect((new UserPolicy)->viewAny($this->user))->toBeFalse();
+});
+
+test('usuário sem permissão não pode atualizar um usuário', function () {
+    expect((new UserPolicy)->update($this->user))->toBeFalse();
+});
+
+// Happy path
+test('permissão de listagem dos usuários é persistida em cache por 5 segundos', function () {
+    grantPermission(User::VIEWANY);
+
+    $key = authenticatedUser()->username . User::VIEWANY;
+
+    expect(Cache::missing($key))->toBeTrue()
+    ->and((new UserPolicy)->viewAny($this->user))->toBeTrue()
+    ->and(Cache::has($key))->toBeTrue()
+    ->and(Cache::get($key))->toBeTrue();
+
+    revokePermission(User::VIEWANY);
+
+    // permissão ainda está em cache
+    expect(Cache::has($key))->toBeTrue()
+    ->and(Cache::get($key))->toBeTrue()
+    ->and((new UserPolicy)->viewAny($this->user))->toBeTrue();
+
+    // expira o cache
+    $this->travel(6)->seconds();
+
+    expect(Cache::missing($key))->toBeTrue()
+    ->and((new UserPolicy)->viewAny($this->user))->toBeFalse()
+    ->and(Cache::has($key))->toBeTrue()
+    ->and(Cache::get($key))->toBeFalse();
+});
+
+test('permissão de atualizar individualmente um usuário é persistida em cache por 5 segundos', function () {
+    grantPermission(User::UPDATE);
+
+    $key = authenticatedUser()->username . User::UPDATE;
+
+    expect(Cache::missing($key))->toBeTrue()
+    ->and((new UserPolicy)->update($this->user))->toBeTrue()
+    ->and(Cache::has($key))->toBeTrue()
+    ->and(Cache::get($key))->toBeTrue();
+
+    revokePermission(User::UPDATE);
+
+    // permissão ainda está em cache
+    expect(Cache::has($key))->toBeTrue()
+    ->and(Cache::get($key))->toBeTrue()
+    ->and((new UserPolicy)->update($this->user))->toBeTrue();
+
+    // expira o cache
+    $this->travel(6)->seconds();
+
+    expect(Cache::missing($key))->toBeTrue()
+    ->and((new UserPolicy)->update($this->user))->toBeFalse()
+    ->and(Cache::has($key))->toBeTrue()
+    ->and(Cache::get($key))->toBeFalse();
+});
+
+test('usuário com permissão pode listar os usuários', function () {
+    grantPermission(User::VIEWANY);
+
+    expect((new UserPolicy)->viewAny($this->user))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar individualmente um usuário', function () {
+    grantPermission(User::UPDATE);
+
+    expect((new UserPolicy)->update($this->user))->toBeTrue();
+});
