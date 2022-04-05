1f503efe6de434c4670be459f5a6b0f8dfcac3af
FCNO <fcno.dev@gmail.com>
2022-04-05 16:48:46 -0300

    chore(job): add enum dos tipos de importações

---
+++ app/Enums/ImportationType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Enums;
+
+use App\Enums\QueueType;
+
+/*
+ * Tipos de importação que a aplicação pode fazer.
+ *
+ * @see https://www.php.net/manual/en/language.enumerations.php
+ * @see https://laravel.com/docs/9.x/collections
+ */
+enum ImportationType: string
+{
+    case Corporate = 'corporate';
+    case PrintLog = 'print_log';
+    /**
+     * Nome para exibição do tipo de importação.
+     *
+     * @return string
+     */
+    public function label()
+    {
+        return match ($this) {
+            ImportationType::Corporate => __('Corporate structure'),
+            ImportationType::PrintLog => __('Print log')
+        };
+    }
+
+    /**
+     * Nome para exibição da queue que será usada para o tipo de importação.
+     *
+     * @return string
+     */
+    public function queue()
+    {
+        return match ($this) {
+            ImportationType::Corporate => QueueType::Corporate->value,
+            ImportationType::PrintLog => QueueType::PrintLog->value
+        };
+    }
+
+    /**
+     * Todos os valores possíveis para o value do enum.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function values()
+    {
+        return
+        collect(QueueType::cases())
+        ->transform(function ($type) {
+            return $type->value;
+        });
+    }
+}
---
+++ tests/Unit/App/Enums/ImportationTypeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Enums\ImportationType;
+
+test('ImportationType enum corretamente definidos', function () {
+    expect(ImportationType::Corporate->value)->toBe('corporate')
+    ->and(ImportationType::PrintLog->value)->toBe('print_log');
+});
+
+test('ImportationType enum values definido', function () {
+    expect(ImportationType::values()->toArray())->toBe(['corporate', 'print_log']);
+});
+
+test('ImportationType enum label definido', function () {
+    expect(ImportationType::Corporate->label())->toBe(__('Corporate structure'))
+    ->and(ImportationType::PrintLog->label())->toBe(__('Print log'));
+});
+
+test('ImportationType enum queue definido', function () {
+    expect(ImportationType::Corporate->queue())->toBe('corporate')
+    ->and(ImportationType::PrintLog->queue())->toBe(__('print_log'));
+});
