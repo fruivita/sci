2ca42354cc627bbeddff3daee9c63aa7e81fe3f1
FCNO <fcno.dev@gmail.com>
2022-04-04 21:26:20 -0300

    chore(policies): registra a policy de usuário

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -19,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         \App\Models\Permission::class => \App\Policies\PermissionPolicy::class,
         \App\Models\Role::class => \App\Policies\RolePolicy::class,
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
     ];
 
     /**
