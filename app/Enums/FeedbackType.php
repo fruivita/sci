d4b662beb8559da0a0c1db59f5d278c576efa3dd
FCNO <fcno.dev@gmail.com>
2022-04-05 22:11:50 -0300

    chore(core): add label e icon no enum feedback

--- app/Enums/FeedbackType.php
+++ app/Enums/FeedbackType.php
@@ -11,4 +11,29 @@ enum FeedbackType: string
 {
     case Success = 'success';
     case Error = 'error';
+    /**
+     * Nome para exibição do tipo de mensagem.
+     *
+     * @return string
+     */
+    public function label()
+    {
+        return match ($this) {
+            FeedbackType::Error => __('Error!'),
+            FeedbackType::Success => __('Success!')
+        };
+    }
+
+    /**
+     * Icone svg para cada tipo de mensagem.
+     *
+     * @return string
+     */
+    public function icon()
+    {
+        return match ($this) {
+            FeedbackType::Error => svg('emoji-frown')->toHtml(),
+            FeedbackType::Success => svg('emoji-smile')->toHtml(),
+        };
+    }
 }
--- tests/Unit/App/Enums/FeebackTypeTest.php
+++ tests/Unit/App/Enums/FeebackTypeTest.php
@@ -11,3 +11,13 @@ test('FeedbackType enum corretamente definidos', function () {
     expect(FeedbackType::Success->value)->toBe('success')
     ->and(FeedbackType::Error->value)->toBe('error');
 });
+
+test('FeedbackType enum label definido', function () {
+    expect(FeedbackType::Success->label())->toBe(__('Success!'))
+    ->and(FeedbackType::Error->label())->toBe(__('Error!'));
+});
+
+test('FeedbackType enum icons definido', function () {
+    expect(FeedbackType::Success->icon())->toContain('emoji-smile', '<svg class="icon"', '</svg>')
+    ->and(FeedbackType::Error->icon())->toContain('emoji-frown', '<svg class="icon"', '</svg>');
+});
