584a9fb63019e1a90c252ba46b7f4d6a94806155
FCNO <fcno.dev@gmail.com>
2022-04-12 01:21:55 -0300

    chore(policies): add policy de delegação de perfil
    Inclusão de métodos para policies de delegação na policy de usuário.

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -33,4 +33,52 @@ class UserPolicy extends Policy
     {
         return $this->hasPermissionWithCache($user, PermissionType::UserUpdate);
     }
+
+    /**
+     * Determine whether the user can view any models to delegate his role.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function delegationViewAny(User $user)
+    {
+        return $this->hasPermissionWithCache($user, PermissionType::DelegationViewAny);
+    }
+
+    /**
+     * Determine whether the user can delegate his role.
+     *
+     * @param \App\Models\User $user
+     * @param \App\Models\User $delegated
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function delegationCreate(User $user, User $delegated)
+    {
+        return
+            !empty($user->role_id)
+            // usuário autenticado possui mais permissões que o destinatário
+            && $user->role_id < $delegated->role_id
+            // possuem a mesma lotação
+            && $user->department_id == $delegated->department_id;
+    }
+
+    /**
+     * Determine whether the user can revoke his delegation.
+     *
+     * @param \App\Models\User $user
+     * @param \App\Models\User $delegated
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delegationDelete(User $user, User $delegated)
+    {
+        return
+            !empty($delegated->role_granted_by)
+            // usuário autenticado possui mais ou o mesmo nível de permissões que o destinatário
+            && $user->role_id <= $delegated->role_id
+            // possuem a mesma lotação
+            && $user->department_id == $delegated->department_id;
+    }
 }
--- tests/Feature/Policies/UserPolicyTest.php
+++ tests/Feature/Policies/UserPolicyTest.php
@@ -5,6 +5,8 @@
  */
 
 use App\Enums\PermissionType;
+use App\Models\Department;
+use App\Models\Role;
 use App\Models\User;
 use App\Policies\UserPolicy;
 use Database\Seeders\RoleSeeder;
@@ -27,6 +29,94 @@ test('usuário sem permissão não pode listar os usuários', function () {
 
 test('usuário sem permissão não pode atualizar um usuário', function () {
     expect((new UserPolicy)->update($this->user))->toBeFalse();
+});
+
+test('usuário não pode delegar perfil, se o perfil do destinatário for superior na aplicação', function () {
+    $department_a = Department::factory()->create();
+    $department_b = Department::factory()->create();
+
+    $user_bar = User::factory()->create([
+        'department_id' => $department_b->id,
+        'role_id' => Role::INSTITUTIONALMANAGER
+    ]);
+
+    $this->user->department_id = $department_a->id;
+    $this->user->role_id = Role::ORDINARY;
+    $this->user->save();
+
+    expect((new UserPolicy)->delegationCreate($this->user, $user_bar))->toBeFalse();
+});
+
+test('usuário não pode delegar perfil para usuário de outra lotação', function () {
+    $department_a = Department::factory()->create();
+    $department_b = Department::factory()->create();
+
+    $this->user->department_id = $department_a->id;
+    $this->user->role_id = Role::INSTITUTIONALMANAGER;
+    $this->user->save();
+
+    $user_bar = User::factory()->create([
+        'department_id' => $department_b->id,
+        'role_id' => Role::ORDINARY
+    ]);
+
+    expect((new UserPolicy)->delegationCreate($this->user, $user_bar))->toBeFalse();
+});
+
+test('usuário não pode remover delegação inexistente', function () {
+    $department = Department::factory()->create();
+
+    $this->user->department_id = $department->id;
+    $this->user->role_id = Role::ADMINISTRATOR;
+    $this->user->save();
+
+    $user_bar = User::factory()->create([
+        'department_id' => $department->id,
+        'role_id' => Role::INSTITUTIONALMANAGER
+    ]);
+
+    expect((new UserPolicy)->delegationDelete($this->user, $user_bar))->toBeFalse();
+});
+
+test('usuário não pode remover delegação de perfil superior', function () {
+    $department = Department::factory()->create();
+
+    $this->user->department_id = $department->id;
+    $this->user->role_id = Role::INSTITUTIONALMANAGER;
+    $this->user->save();
+
+    $user_bar = User::factory()->create([
+        'department_id' => $department->id,
+        'role_id' => Role::ADMINISTRATOR
+    ]);
+    $user_taz = User::factory()->create([
+        'department_id' => $department->id,
+        'role_id' => Role::ADMINISTRATOR,
+        'role_granted_by' => $user_bar->id
+    ]);
+
+    expect((new UserPolicy)->delegationDelete($this->user, $user_taz))->toBeFalse();
+});
+
+test('usuário não pode remover delegação de usuário de outra lotação', function () {
+    $department_a = Department::factory()->create();
+    $department_b = Department::factory()->create();
+
+    $this->user->department_id = $department_a->id;
+    $this->user->role_id = Role::ADMINISTRATOR;
+    $this->user->save();
+
+    $user_bar = User::factory()->create([
+        'department_id' => $department_b->id,
+        'role_id' => Role::INSTITUTIONALMANAGER
+    ]);
+    $user_taz = User::factory()->create([
+        'department_id' => $department_b->id,
+        'role_id' => Role::INSTITUTIONALMANAGER,
+        'role_granted_by' => $user_bar->id
+    ]);
+
+    expect((new UserPolicy)->delegationDelete($this->user, $user_taz))->toBeFalse();
 });
 
 // Happy path
@@ -92,4 +182,41 @@ test('usuário com permissão pode atualizar individualmente um usuário', funct
     grantPermission(PermissionType::UserUpdate->value);
 
     expect((new UserPolicy)->update($this->user))->toBeTrue();
+});
+
+test('usuário pode delegar perfil dentro da mesma lotação, se o perfil do destinatário for inferior na aplicação', function () {
+    $department = Department::factory()->create();
+
+    $this->user->department_id = $department->id;
+    $this->user->role_id = Role::INSTITUTIONALMANAGER;
+    $this->user->save();
+
+    $user_bar = User::factory()->create([
+        'department_id' => $department->id,
+        'role_id' => Role::ORDINARY
+    ]);
+
+    expect((new UserPolicy)->delegationCreate($this->user, $user_bar))->toBeTrue();
+});
+
+test('usuário pode remover delegação de usuário da mesma lotação, com perfil igual ou inferior, mesmo delegado por outrem', function () {
+    $department = Department::factory()->create();
+
+    $this->user->department_id = $department->id;
+    $this->user->role_id = Role::INSTITUTIONALMANAGER;
+    $this->user->save();
+
+    $user_bar = User::factory()->create([
+        'department_id' => $department->id,
+        'role_id' => Role::INSTITUTIONALMANAGER
+    ]);
+
+    $user_baz = User::factory()->create([
+        'department_id' => $department->id,
+        'role_id' => Role::INSTITUTIONALMANAGER,
+        'role_granted_by' => $user_bar->id,
+    ]);
+
+    expect((new UserPolicy)->delegationDelete($this->user, $user_bar))->toBeTrue()
+    ->and((new UserPolicy)->delegationDelete($this->user, $user_baz))->toBeTrue();
 });
