4f5f6fe4be8d82c2de85eddd042b56cd0b030737
FCNO <fcno.dev@gmail.com>
2022-04-11 22:08:30 -0300

    chore(policies): add tipo de action da policy

--- tests/Unit/App/Enums/PolicyActionTest.php
+++ tests/Unit/App/Enums/PolicyActionTest.php
@@ -17,5 +17,8 @@ test('Policy enum corretamente definidos', function () {
     ->and(Policy::ForceDelete->value)->toBe('force-delete')
     ->and(Policy::SimulationCreate->value)->toBe('simulation-create')
     ->and(Policy::SimulationDelete->value)->toBe('simulation-delete')
-    ->and(Policy::ImportationCreate->value)->toBe('importation-create');
+    ->and(Policy::ImportationCreate->value)->toBe('importation-create')
+    ->and(Policy::DelegationViewAny->value)->toBe('delegation-view-any')
+    ->and(Policy::DelegationCreate->value)->toBe('delegation-create')
+    ->and(Policy::DelegationDelete->value)->toBe('delegation-delete');
 });
