b78f2ae31fcba5b3ca97a9ecf8b129e7103d427c
FCNO <fcno.dev@gmail.com>
2022-04-16 17:19:15 -0300

    chore(core): trait para download de relatório pdf

---
+++ app/Http/Livewire/Traits/WithDownloadableReport.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Livewire\Traits;
+
+use Barryvdh\DomPDF\Facade\Pdf;
+
+/**
+ * Trait para as tratativas de download do relatório.
+ *
+ * @see https://www.php.net/manual/en/language.oop5.traits.php
+ * @see https://laravel-livewire.com/docs/2.x/traits
+ */
+trait WithDownloadableReport
+{
+    /**
+     * Título do relatório que será gerado.
+     *
+     * @return string
+     */
+    abstract private function reportHeader();
+
+    /**
+     * Nome da view utilizada para a geração do relatório.
+     *
+     * @return string
+     */
+    abstract private function reportViewName();
+
+    /**
+     * Download do relatório em formatro PDF.
+     *
+     * @return void
+     */
+    public function downloadPDFReport()
+    {
+        $this->validate();
+
+        $file_name = str('report-')
+        ->append(now()->format('d-m-Y'))
+        ->finish('.pdf');
+
+        $headers = array(
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => "attachment; filename={$file_name}",
+        );
+
+        $pdf_content = Pdf::loadView(
+            $this->reportViewName(),
+            $this->data()
+        )->output();
+
+        return response()->streamDownload(
+            fn () => print($pdf_content),
+            $file_name,
+            $headers
+        );
+    }
+
+    /**
+     * Dados para serem populados no relatório.
+     *
+     * @return array<string, mixed>
+     */
+    private function data()
+    {
+        return [
+            'header' => $this->reportHeader(),
+            'initial_date' => $this->initial_date,
+            'final_date' => $this->final_date,
+            'filter' => $this->term,
+            'result' => $this->reportQuery()->get(),
+        ];
+    }
+}
