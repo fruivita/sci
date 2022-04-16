c6aab62cc97c0b5aa48d8450f12bcf621ceae7d7
FCNO <fcno.dev@gmail.com>
2022-04-15 23:38:00 -0300

    chore: import packages flastpickr e alpine-mask
    Importa os packages no arquivo css e no javascript

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,8 @@
-import Alpine from 'alpinejs'
-
+import Alpine from 'alpinejs';
+import Mask from "@ryangjchandler/alpine-mask";
+Alpine.plugin(Mask);
 window.Alpine = Alpine
-
 Alpine.start()
+
+import flatpickr from "flatpickr";
+import pt from 'flatpickr/dist/l10n/pt';
