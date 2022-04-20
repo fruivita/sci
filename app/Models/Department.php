8c4f04890fa4217233be661d9264235ab68815f5
FCNO <fcno.dev@gmail.com>
2022-04-20 01:14:39 -0300

    chore(model): cria relatórios por lotação

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,7 +2,11 @@
 
 namespace App\Models;
 
+use App\Enums\DepartmentReportType;
+use Carbon\Carbon;
 use FruiVita\Corporate\Models\Department as CorporateDepartment;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Lotação de um determinado usuário.
@@ -21,5 +25,278 @@ class Department extends CorporateDepartment
     public function prints()
     {
         return $this->hasMany(Printing::class, 'department_id', 'id');
+    }
+
+    /**
+     * Gera o relatório de impressão por lotação de acordo com o período
+     * informado.
+     *
+     * O relatório traz as seguintes informações de acordo com os parâmetros
+     * informados:
+     * - department: nome da lotação
+     * - acronym: sigla da lotação
+     * - total_print: volume de impressão
+     * - printer_count: quantidade de impressoras utilizadas
+     * - parent_acronym: sigla da lotação pai
+     * - parent_department: nome da lotação pai
+     *
+     * O retorno é ordenado pelo:
+     * - total_print desc
+     * - department asc
+     *
+     * Regra de negócio: Existem 3 tipos de relatórios de impressão por lotação
+     * - intitucional: relatório de todas as lotações cadastradas.
+     * - gerencial: relatório da lotação da pessoa autenticada e lotações filha
+     * - lotação: relatório apenas da lotação da pessoa autenticada.
+     *
+     * O relatório inclui as lotações com impressões zeradas.
+     *
+     * @param \Carbon\Carbon                 $initial_date
+     * @param \Carbon\Carbon                 $final_date
+     * @param \App\Enum\DepartmentReportType $type
+     * @param int                            $per_page
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public static function report(Carbon $initial_date, Carbon $final_date, DepartmentReportType $type, int $per_page)
+    {
+        return
+        self::{$type->value}(
+            $initial_date->startOfDay(),
+            $final_date->endOfDay(),
+            $per_page
+        );
+    }
+
+    /**
+     * Gera o relatório intitucional de impressão por lotação de acordo com o
+     * período.
+     *
+     * O relatório traz as seguintes informações de acordo com os parâmetros
+     * informados:
+     * - department: nome da lotação
+     * - acronym: sigla da lotação
+     * - total_print: volume de impressão
+     * - printer_count: quantidade de impressoras utilizadas
+     * - parent_acronym: sigla da lotação pai
+     * - parent_department: nome da lotação pai
+     *
+     * O retorno é ordenado pelo:
+     * - total_print desc
+     * - department asc
+     *
+     * Regra de negócio: o relatório do tipo 'institucional' traz informações
+     * sobre todas as lotações existentes, inclusive, as com impressão zerada.
+     *
+     * @param \Carbon\Carbon $initial_date
+     * @param \Carbon\Carbon $final_date
+     * @param int            $per_page
+     *
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     */
+    private static function institutional(Carbon $initial_date, Carbon $final_date, int $per_page)
+    {
+        //prepara-se todas as impressoes e quantidades de impressoras
+        //utilizadas no período informado.
+        $prints = DB::table('prints')
+            ->selectRaw('department_id, SUM(copies * pages) AS total_print, COUNT(DISTINCT printer_id) AS printer_count')
+            ->whereBetween('date', [$initial_date, $final_date])
+            ->groupBy('department_id');
+
+        //prepara-se todas as locações com a sigla da lotação pai right
+        $department = DB::table('departments', 'd1')
+            ->select('d1.acronym', 'd1.name AS department', 'tmp.total_print', 'tmp.printer_count', 'd2.acronym AS parent_acronym', 'd2.name AS parent_department')
+            ->leftJoin('departments AS d2', 'd2.id', '=', 'd1.parent_department')
+            ->rightJoinSub($prints, 'tmp', 'tmp.department_id', '=', 'd1.id');
+
+        //junta as query e a prepara para a execução.
+        return
+        DB::table('departments', 'd3')
+            ->select('d3.acronym', 'd3.name AS department', 'tmp.total_print', 'tmp.printer_count', 'd4.acronym AS parent_acronym', 'd4.name AS parent_department')
+            ->leftJoin('departments AS d4', 'd4.id', '=', 'd3.parent_department')
+            ->leftJoinSub($prints, 'tmp', 'tmp.department_id', '=', 'd3.id')
+            ->union($department)
+            ->orderBy('total_print', 'desc')
+            ->orderBy('department', 'asc')
+            ->paginate($per_page);
+    }
+
+    /**
+     * Gera o relatório gerencial de impressão por lotação de acordo com o
+     * período.
+     *
+     * O relatório traz as seguintes informações de acordo com os parâmetros
+     * informados:
+     * - department: nome da lotação
+     * - acronym: sigla da lotação
+     * - total_print: volume de impressão
+     * - printer_count: quantidade de impressoras utilizadas
+     * - parent_acronym: sigla da lotação pai
+     * - parent_department: nome da lotação pai
+     *
+     * O retorno é ordenado pelo:
+     * - total_print desc
+     * - department asc
+     *
+     * Regra de negócio: o relatório do tipo 'gerencial' traz informações
+     * da lotação atual da pessoa autenticada bem como das suas filhas.
+     *
+     * @param \Carbon\Carbon $initial_date
+     * @param \Carbon\Carbon $final_date
+     * @param int            $per_page
+     *
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     *
+     * @see \App\Providers\AppServiceProvider
+     * @see https://gist.github.com/simonhamp/549e8821946e2c40a617c85d2cf5af5e
+     */
+    private static function managerial(Carbon $initial_date, Carbon $final_date, int $per_page)
+    {
+        $page = Paginator::resolveCurrentPage() ?: 1;
+        $offset = ($page * $per_page) - $per_page;
+
+        $result = DB::select(
+            'WITH RECURSIVE CTE AS
+            (
+                SELECT
+                    d1.id, d1.acronym, d1.name, d1.parent_department
+                FROM
+                    departments d1
+                WHERE d1.id=(
+                    SELECT
+                        d2.id
+                    FROM
+                        departments d2
+                    JOIN users u ON u.department_id = d2.id
+                    WHERE u.username = ?)
+
+                UNION ALL
+
+                SELECT
+                    d3.id, d3.acronym, d3.name, d3.parent_department
+                FROM
+                    CTE lCTE, departments d3
+                WHERE d3.parent_department = lCTE.id
+            )
+
+            SELECT
+                lCTE.id,
+                lCTE.acronym,
+                lCTE.name AS department,
+                d4.acronym AS parent_acronym,
+                d4.name AS parent_department,
+                COALESCE(SUM(i.copies * i.pages), 0) AS total_print,
+                COUNT(DISTINCT i.printer_id) AS printer_count
+            FROM CTE lCTE
+            LEFT JOIN departments d4 ON d4.id = lCTE.parent_department
+            LEFT JOIN prints i ON i.department_id = lCTE.id
+                AND (i.date IS NULL OR i.date BETWEEN ? AND ?)
+            GROUP BY lCTE.id, lCTE.acronym, lCTE.name, d4.acronym, d4.name
+            ORDER BY total_print DESC, department ASC
+            LIMIT ? OFFSET ?',
+            [
+                auth()->user()->username,
+                $initial_date,
+                $final_date,
+                $per_page,
+                $offset,
+            ]
+        );
+
+        return
+        collect($result)
+        ->customPaginate(
+            self::managerialTotalRecords($initial_date, $final_date),
+            $per_page,
+            $page
+        );
+    }
+
+    /**
+     * Gera o relatório de impressão da lotação do usuário autenticado.
+     *
+     * O relatório traz as seguintes informações de acordo com os parâmetros
+     * informados:
+     * - department: nome da lotação
+     * - acronym: sigla da lotação
+     * - total_print: volume de impressão
+     * - printer_count: quantidade de impressoras utilizadas
+     * - parent_acronym: sigla da lotação pai
+     * - parent_department: nome da lotação pai
+     *
+     * Regra de negócio: o relatório do tipo 'lotação' traz informações apenas
+     * da lotação da pessoa autenticada, não traz das lotações filha.
+     *
+     * @param \Carbon\Carbon $initial_date
+     * @param \Carbon\Carbon $final_date
+     * @param int            $per_page
+     *
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     */
+    private static function department(Carbon $initial_date, Carbon $final_date, int $per_page)
+    {
+        return
+        DB::table('prints', 'i')
+            ->selectRaw('SUM(i.copies * i.pages) AS total_print, d.acronym, d.name AS department, parent.acronym AS parent_acronym, parent.name AS parent_department, COUNT(DISTINCT printer_id) AS printer_count')
+            ->rightJoin('departments AS d', 'd.id', '=', 'i.department_id')
+            ->join('users AS u', 'u.department_id', '=', 'd.id')
+            ->leftJoin('departments AS parent', 'parent.id', '=', 'd.parent_department')
+            ->where('u.username', '=', auth()->user()->username)
+            ->where(function ($query) use ($initial_date, $final_date) {
+                $query->whereBetween('i.date', [$initial_date, $final_date])
+                    ->orWhereNull('i.date');
+            })
+            ->groupBy('u.department_id')
+            ->paginate($per_page);
+    }
+
+    /**
+     * Calcula a quantidade de registros retornada pelo relatório gerencial.
+     *
+     * @param \Carbon\Carbon $initial_date
+     * @param \Carbon\Carbon $final_date
+     *
+     * @return int
+     */
+    private static function managerialTotalRecords(Carbon $initial_date, Carbon $final_date)
+    {
+        $result = DB::select(
+            'WITH RECURSIVE CTE AS
+            (
+                SELECT
+                    d1.id, d1.parent_department
+                FROM
+                    departments d1
+                WHERE d1.id=(
+                    SELECT
+                        d2.id
+                    FROM
+                        departments d2
+                    JOIN users u ON u.department_id = d2.id
+                    WHERE u.username = ?)
+
+                UNION ALL
+
+                SELECT
+                    d3.id, d3.parent_department
+                FROM
+                    CTE lCTE, departments d3
+                WHERE d3.parent_department = lCTE.id
+            )
+
+            SELECT
+                count(1) AS total
+            FROM CTE lCTE
+            LEFT JOIN departments d4 ON d4.id = lCTE.parent_department
+            LEFT JOIN prints i ON i.department_id = lCTE.id
+                AND (i.date IS NULL OR i.date BETWEEN ? AND ?)',
+            [
+                auth()->user()->username,
+                $initial_date,
+                $final_date,
+            ]
+        );
+
+        return (int) $result[0]->total;
     }
 }
---
+++ tests/Unit/App/Models/Reports/DepartmentReportTest.php
@@ -0,0 +1,384 @@
+<?php
+
+/**
+ * As lotações e usuários usados nestes testes, são os previstos no template.
+ *
+ * @see https://pestphp.com/docs/
+ * @see \testes\template\Corporate.xml
+ */
+
+use App\Enums\DepartmentReportType;
+use App\Importer\PrintLogImporter;
+use App\Jobs\ImportCorporateStructure;
+use App\Models\Department;
+use Database\Seeders\RoleSeeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+
+beforeEach(function () {
+    $this->seed(RoleSeeder::class);
+
+    $this->print_log_files = [
+        '30-06-2019.txt' => 'server1.domain.gov.br╡30/06/2019╡01:00:00╡report.pdf╡Sigla 2╡╡2╡╡CPU-10000╡MLT-111╡1000╡3╡1' . PHP_EOL .
+            'server1.domain.gov.br╡30/06/2019╡10:30:00╡private.pdf╡Sigla 3╡╡3╡╡CPU-10000╡IMP-222╡5000╡8╡2' . PHP_EOL,
+
+        '02-12-2020.txt' => 'server1.domain.gov.br╡02/12/2020╡02:05:00╡report.pdf╡Sigla 2╡╡2╡╡CPU-10000╡IMP-666╡3000╡10╡2' . PHP_EOL .
+            'server1.domain.gov.br╡02/12/2020╡13:15:15╡games.pdf╡Sigla 1╡╡1╡╡CPU-20000╡IMP-444╡1000╡3╡1' . PHP_EOL .
+            'server1.domain.gov.br╡02/12/2020╡18:01:50╡rules.pdf╡Sigla 1╡╡1╡╡CPU-20000╡MLT-111╡2000╡9╡2' . PHP_EOL,
+
+        '05-12-2020.txt' => 'server1.domain.gov.br╡05/12/2020╡03:00:00╡report.pdf╡Sigla 2╡╡2╡╡CPU-10000╡IMP-555╡3000╡5╡3' . PHP_EOL .
+            'server1.domain.gov.br╡05/12/2020╡13:15:15╡games.pdf╡Sigla 1╡╡1╡╡CPU-20000╡IMP-444╡1000╡5╡7' . PHP_EOL .
+            'server1.domain.gov.br╡05/12/2020╡18:01:50╡rules.pdf╡Sigla 1╡╡1╡╡CPU-20000╡MLT-111╡2000╡3╡2' . PHP_EOL,
+
+        '25-12-2020.txt' => 'server1.domain.gov.br╡25/12/2020╡03:30:00╡report.pdf╡Sigla 2╡╡2╡╡CPU-10000╡MLT-333╡3000╡1╡1' . PHP_EOL .
+            'server1.domain.gov.br╡25/12/2020╡13:15:15╡games.pdf╡Sigla 1╡╡1╡╡CPU-20000╡IMP-222╡1000╡4╡1' . PHP_EOL .
+            'server1.domain.gov.br╡25/12/2020╡18:01:50╡rules.pdf╡Sigla 1╡╡1╡╡CPU-20000╡MLT-111╡2000╡9╡2' . PHP_EOL,
+    ];
+
+    $this->fake_disk = Storage::fake('log-impressao');
+
+    foreach ($this->print_log_files as $filename => $content) {
+        $this->fake_disk->put($filename, $content);
+    }
+});
+
+afterEach(function () {
+    $this->fake_disk = Storage::fake('log-impressao');
+});
+
+test('relatório institucional traz informações sobre todas as lotações', function () {
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '30-06-2019'),
+        Carbon::createFromFormat('d-m-Y', '25-12-2020'),
+        DepartmentReportType::Institutional,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+    $department2 = $result->firstWhere('department', 'Lotação 2');
+    $department3 = $result->firstWhere('department', 'Lotação 3');
+    $department4 = $result->firstWhere('department', 'Lotação 4');
+    $department5 = $result->firstWhere('department', 'Lotação 5');
+
+    expect($result)->toHaveCount(5)
+    ->and($department1->acronym)->toBe('Sigla 1')
+    ->and($department1->total_print)->toBe('84')
+    ->and($department1->printer_count)->toBe(3)
+    ->and($department1->parent_acronym)->toBeNull()
+    ->and($department1->parent_department)->toBeNull()
+    ->and($department2->acronym)->toBe('Sigla 2')
+    ->and($department2->total_print)->toBe('39')
+    ->and($department2->printer_count)->toBe(4)
+    ->and($department2->parent_acronym)->toBeNull()
+    ->and($department2->parent_department)->toBeNull()
+    ->and($department3->acronym)->toBe('Sigla 3')
+    ->and($department3->total_print)->toBe('16')
+    ->and($department3->printer_count)->toBe(1)
+    ->and($department3->parent_acronym)->toBe('Sigla 1')
+    ->and($department3->parent_department)->toBe('Lotação 1')
+    ->and($department4->acronym)->toBe('Sigla 4')
+    ->and($department4->total_print)->toBeNull()
+    ->and($department4->printer_count)->toBeNull()
+    ->and($department4->parent_acronym)->toBeNull()
+    ->and($department4->parent_department)->toBeNull()
+    ->and($department5->acronym)->toBe('Sigla 5')
+    ->and($department5->total_print)->toBeNull()
+    ->and($department5->printer_count)->toBeNull()
+    ->and($department5->parent_acronym)->toBe('Sigla 1')
+    ->and($department5->parent_department)->toBe('Lotação 1');
+});
+
+test('relatório gerencial traz informações sobre pai e as filhas', function () {
+    login('Sigla 1');
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '30-06-2019'),
+        Carbon::createFromFormat('d-m-Y', '25-12-2020'),
+        DepartmentReportType::Managerial,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+    $department3 = $result->firstWhere('department', 'Lotação 3');
+    $department5 = $result->firstWhere('department', 'Lotação 5');
+
+    expect($result)->toHaveCount(3)
+    ->and($department1->acronym)->toBe('Sigla 1')
+    ->and($department1->total_print)->toBe('84')
+    ->and($department1->printer_count)->toBe(3)
+    ->and($department1->parent_acronym)->toBeNull()
+    ->and($department1->parent_department)->toBeNull()
+    ->and($department3->acronym)->toBe('Sigla 3')
+    ->and($department3->total_print)->toBe('16')
+    ->and($department3->printer_count)->toBe(1)
+    ->and($department3->parent_acronym)->toBe('Sigla 1')
+    ->and($department3->parent_department)->toBe('Lotação 1')
+    ->and($department5->acronym)->toBe('Sigla 5')
+    ->and($department5->total_print)->toBe('0')
+    ->and($department5->printer_count)->toBe(0)
+    ->and($department5->parent_acronym)->toBe('Sigla 1')
+    ->and($department5->parent_department)->toBe('Lotação 1');
+
+    logout();
+});
+
+test('relatório gerencial não traz informação sobre lotação pai a partir da lotação filha', function () {
+    login('Sigla 3'); // usuário lotado na lotação 3, filha da lotação 1
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '30-06-2019'),
+        Carbon::createFromFormat('d-m-Y', '25-12-2020'),
+        DepartmentReportType::Managerial,
+        9999
+    );
+
+    $department3 = $result->firstWhere('department', 'Lotação 3');
+
+    expect($result)->toHaveCount(1)
+    ->and($department3->acronym)->toBe('Sigla 3')
+    ->and($department3->total_print)->toBe('16')
+    ->and($department3->printer_count)->toBe(1)
+    ->and($department3->parent_acronym)->toBe('Sigla 1')
+    ->and($department3->parent_department)->toBe('Lotação 1');
+
+    logout();
+});
+
+test('relatório por lotação traz informações apenas da lotação da pessoa autenticada', function () {
+    login('Sigla 1'); // usuário lotado na lotação 1, mas não trará nenhuma das filhas
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '30-06-2019'),
+        Carbon::createFromFormat('d-m-Y', '25-12-2020'),
+        DepartmentReportType::Department,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+
+    expect($result)->toHaveCount(1)
+    ->and($department1->acronym)->toBe('Sigla 1')
+    ->and($department1->total_print)->toBe('84')
+    ->and($department1->printer_count)->toBe(3)
+    ->and($department1->parent_acronym)->toBeNull()
+    ->and($department1->parent_department)->toBeNull();
+});
+
+test('relatório institucional com restrição de período', function () {
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '01-12-2020'),
+        Carbon::createFromFormat('d-m-Y', '15-12-2020'),
+        DepartmentReportType::Institutional,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+    $department2 = $result->firstWhere('department', 'Lotação 2');
+    $department3 = $result->firstWhere('department', 'Lotação 3');
+    $department4 = $result->firstWhere('department', 'Lotação 4');
+    $department5 = $result->firstWhere('department', 'Lotação 5');
+
+    expect($result)->toHaveCount(5)
+    ->and($department1->total_print)->toBe('62')
+    ->and($department1->printer_count)->toBe(2)
+    ->and($department2->total_print)->toBe('35')
+    ->and($department2->printer_count)->toBe(2)
+    ->and($department3->total_print)->toBeNull()
+    ->and($department3->printer_count)->toBeNull()
+    ->and($department4->total_print)->toBeNull()
+    ->and($department4->printer_count)->toBeNull()
+    ->and($department5->total_print)->toBeNull()
+    ->and($department5->printer_count)->toBeNull();
+});
+
+test('relatório gerencial com restrição de período', function () {
+    login('Sigla 1');
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '01-12-2020'),
+        Carbon::createFromFormat('d-m-Y', '15-12-2020'),
+        DepartmentReportType::Managerial,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+    $department3 = $result->firstWhere('department', 'Lotação 3');
+    $department5 = $result->firstWhere('department', 'Lotação 5');
+
+    expect($result)->toHaveCount(3)
+    ->and($department1->total_print)->toBe('62')
+    ->and($department1->printer_count)->toBe(2)
+    ->and($department3->total_print)->toBe('0')
+    ->and($department3->printer_count)->toBe(0)
+    ->and($department5->total_print)->toBe('0')
+    ->and($department5->printer_count)->toBe(0);
+    logout();
+});
+
+test('relatório por lotação com restrição de período', function () {
+    login('Sigla 1');
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '01-12-2020'),
+        Carbon::createFromFormat('d-m-Y', '15-12-2020'),
+        DepartmentReportType::Department,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+
+    expect($result)->toHaveCount(1)
+    ->and($department1->total_print)->toBe('62')
+    ->and($department1->printer_count)->toBe(2);
+    logout();
+});
+
+test('relatório institucional, mesmo sem impressão no período, traz o relatório completo', function () {
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '01-08-2021'),
+        Carbon::createFromFormat('d-m-Y', '02-08-2021'),
+        DepartmentReportType::Institutional,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+    $department2 = $result->firstWhere('department', 'Lotação 2');
+    $department3 = $result->firstWhere('department', 'Lotação 3');
+    $department4 = $result->firstWhere('department', 'Lotação 4');
+    $department5 = $result->firstWhere('department', 'Lotação 5');
+
+    expect($result)->toHaveCount(5)
+    ->and($department1->total_print)->toBeNull()
+    ->and($department1->printer_count)->toBeNull()
+    ->and($department2->total_print)->toBeNull()
+    ->and($department2->printer_count)->toBeNull()
+    ->and($department3->total_print)->toBeNull()
+    ->and($department3->printer_count)->toBeNull()
+    ->and($department4->total_print)->toBeNull()
+    ->and($department4->printer_count)->toBeNull()
+    ->and($department5->total_print)->toBeNull()
+    ->and($department5->printer_count)->toBeNull();
+});
+
+test('relatório gerencial, mesmo sem impressão no período, traz o relatório completo', function () {
+    login('Sigla 1');
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '01-08-2021'),
+        Carbon::createFromFormat('d-m-Y', '02-08-2021'),
+        DepartmentReportType::Managerial,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+    $department3 = $result->firstWhere('department', 'Lotação 3');
+    $department5 = $result->firstWhere('department', 'Lotação 5');
+
+    expect($result)->toHaveCount(3)
+    ->and($department1->total_print)->toBe('0')
+    ->and($department1->printer_count)->toBe(0)
+    ->and($department3->total_print)->toBe('0')
+    ->and($department3->printer_count)->toBe(0)
+    ->and($department5->total_print)->toBe('0')
+    ->and($department5->printer_count)->toBe(0);
+
+    logout();
+});
+
+test('relatório por lotação, mesmo sem impressão no período, traz o relatório completo', function () {
+    login('Sigla 1');
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '01-08-2021'),
+        Carbon::createFromFormat('d-m-Y', '02-08-2021'),
+        DepartmentReportType::Department,
+        9999
+    );
+
+    $department1 = $result->firstWhere('department', 'Lotação 1');
+
+    expect($result)->toHaveCount(1)
+    ->and($department1->total_print)->toBe('0')
+    ->and($department1->printer_count)->toBe(0);
+    logout();
+})->skip(true);
+
+test('relatório institucional é ordenado pelo volume impressão desc e lotação asc', function () {
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '30-06-2019'),
+        Carbon::createFromFormat('d-m-Y', '25-12-2020'),
+        DepartmentReportType::Institutional,
+        9999
+    );
+
+    $first = $result->get(0);
+    $second = $result->get(1);
+    $third = $result->get(2);
+    $fourth = $result->get(3);
+    $fifth = $result->get(4);
+
+    expect($result)->toHaveCount(5)
+    ->and($first->department)->toBe('Lotação 1')
+    ->and($first->total_print)->toBe('84')
+    ->and($second->department)->toBe('Lotação 2')
+    ->and($second->total_print)->toBe('39')
+    ->and($third->department)->toBe('Lotação 3')
+    ->and($third->total_print)->toBe('16')
+    ->and($fourth->department)->toBe('Lotação 4')
+    ->and($fourth->total_print)->toBeNull()
+    ->and($fifth->department)->toBe('Lotação 5')
+    ->and($fifth->total_print)->toBeNull();
+});
+
+test('relatório gerencial é ordenado pelo volume impressão desc e lotação asc', function () {
+    login('Sigla 1');
+    ImportCorporateStructure::dispatchSync();
+    PrintLogImporter::make()->import();
+
+    $result = Department::report(
+        Carbon::createFromFormat('d-m-Y', '30-06-2019'),
+        Carbon::createFromFormat('d-m-Y', '25-12-2020'),
+        DepartmentReportType::Managerial,
+        9999
+    );
+
+    $first = $result->get(0);
+    $second = $result->get(1);
+    $third = $result->get(2);
+
+    expect($result)->toHaveCount(3)
+    ->and($first->department)->toBe('Lotação 1')
+    ->and($first->total_print)->toBe('84')
+    ->and($second->department)->toBe('Lotação 3')
+    ->and($second->total_print)->toBe('16')
+    ->and($third->department)->toBe('Lotação 5')
+    ->and($third->total_print)->toBe('0');
+
+    logout();
+});
