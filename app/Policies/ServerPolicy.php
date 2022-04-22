356d0597c0c40000d60c9eb093f26809fda42402
FCNO <fcno.dev@gmail.com>
2022-04-22 16:22:18 -0300

    chore(policies): update server policy
    Adiciona policies ViewAny, View e Update na PolicyServer.

--- app/Policies/ServerPolicy.php
+++ app/Policies/ServerPolicy.php
@@ -11,6 +11,42 @@ use App\Models\User;
 class ServerPolicy extends Policy
 {
     /**
+     * Determine whether the user can view any models.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function viewAny(User $user)
+    {
+        return $this->hasPermissionWithCache($user, PermissionType::ServerViewAny);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function view(User $user)
+    {
+        return $this->hasPermissionWithCache($user, PermissionType::ServerView);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return bool|\Illuminate\Auth\Access\Response
+     */
+    public function update(User $user)
+    {
+        return $this->hasPermissionWithCache($user, PermissionType::ServerUpdate);
+    }
+
+    /**
      * Determine whether the user can generate server report.
      *
      * @param \App\Models\User $user
--- tests/Feature/Policies/ServerPolicyTest.php
+++ tests/Feature/Policies/ServerPolicyTest.php
@@ -22,11 +22,107 @@ afterEach(function () {
 });
 
 // Forbidden
+test('usuário sem permissão não pode listar os servidores', function () {
+    expect((new ServerPolicy)->viewAny($this->user))->toBeFalse();
+});
+
+test('usuário sem permissão não pode visualizar individualmente um servidor', function () {
+    expect((new ServerPolicy)->update($this->user))->toBeFalse();
+});
+
+test('usuário sem permissão não pode atualizar um servidor', function () {
+    expect((new ServerPolicy)->update($this->user))->toBeFalse();
+});
+
 test('usuário sem permissão não pode gerar relatório por servidor', function () {
     expect((new ServerPolicy)->report($this->user))->toBeFalse();
 });
 
 // Happy path
+test('permissão de listagem dos servidores é persistida em cache por 5 segundos', function () {
+    grantPermission(PermissionType::ServerViewAny->value);
+
+    $key = $this->user->username . PermissionType::ServerViewAny->value;
+
+    expect(cache()->missing($key))->toBeTrue()
+    ->and((new ServerPolicy)->viewAny($this->user))->toBeTrue()
+    ->and(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeTrue();
+
+    testTime()->freeze();
+    revokePermission(PermissionType::ServerViewAny->value);
+    testTime()->addSeconds(5);
+
+    // permissão ainda está em cache
+    expect(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeTrue()
+    ->and((new ServerPolicy)->viewAny($this->user))->toBeTrue();
+
+    // expira o cache
+    testTime()->addSeconds(1);
+
+    expect(cache()->missing($key))->toBeTrue()
+    ->and((new ServerPolicy)->viewAny($this->user))->toBeFalse()
+    ->and(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeFalse();
+});
+
+test('permissão de visualizar individualmente um servidor é persistida em cache por 5 segundos', function () {
+    grantPermission(PermissionType::ServerView->value);
+
+    $key = $this->user->username . PermissionType::ServerView->value;
+
+    expect(cache()->missing($key))->toBeTrue()
+    ->and((new ServerPolicy)->view($this->user))->toBeTrue()
+    ->and(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeTrue();
+
+    testTime()->freeze();
+    revokePermission(PermissionType::ServerView->value);
+    testTime()->addSeconds(5);
+
+    // permissão ainda está em cache
+    expect(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeTrue()
+    ->and((new ServerPolicy)->view($this->user))->toBeTrue();
+
+    // expira o cache
+    testTime()->addSeconds(1);
+
+    expect(cache()->missing($key))->toBeTrue()
+    ->and((new ServerPolicy)->view($this->user))->toBeFalse()
+    ->and(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeFalse();
+});
+
+test('permissão de atualizar individualmente um servidor é persistida em cache por 5 segundos', function () {
+    grantPermission(PermissionType::ServerUpdate->value);
+
+    $key = $this->user->username . PermissionType::ServerUpdate->value;
+
+    expect(cache()->missing($key))->toBeTrue()
+    ->and((new ServerPolicy)->update($this->user))->toBeTrue()
+    ->and(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeTrue();
+
+    testTime()->freeze();
+    revokePermission(PermissionType::ServerUpdate->value);
+    testTime()->addSeconds(5);
+
+    // permissão ainda está em cache
+    expect(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeTrue()
+    ->and((new ServerPolicy)->update($this->user))->toBeTrue();
+
+    // expira o cache
+    testTime()->addSeconds(1);
+
+    expect(cache()->missing($key))->toBeTrue()
+    ->and((new ServerPolicy)->update($this->user))->toBeFalse()
+    ->and(cache()->has($key))->toBeTrue()
+    ->and(cache()->get($key))->toBeFalse();
+});
+
 test('permissão de gerar o relatório por servidor é persistida em cache por 5 segundos', function () {
     grantPermission(PermissionType::ServerReport->value);
 
@@ -53,6 +149,24 @@ test('permissão de gerar o relatório por servidor é persistida em cache por 5
     ->and((new ServerPolicy)->report($this->user))->toBeFalse()
     ->and(cache()->has($key))->toBeTrue()
     ->and(cache()->get($key))->toBeFalse();
+});
+
+test('usuário com permissão pode listar os perfis', function () {
+    grantPermission(PermissionType::RoleViewAny->value);
+
+    expect((new ServerPolicy)->viewAny($this->user))->toBeTrue();
+});
+
+test('usuário com permissão pode visualizar individualmente um perfil', function () {
+    grantPermission(PermissionType::RoleView->value);
+
+    expect((new ServerPolicy)->view($this->user))->toBeTrue();
+});
+
+test('usuário com permissão pode atualizar individualmente um perfil', function () {
+    grantPermission(PermissionType::RoleUpdate->value);
+
+    expect((new ServerPolicy)->update($this->user))->toBeTrue();
 });
 
 test('usuário com permissão pode gerar o relatório por servidor', function () {
