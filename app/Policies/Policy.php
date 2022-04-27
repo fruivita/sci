897b5f7ab5ab59455110fd7ba574039f5e526ed8
FCNO <fcno.dev@gmail.com>
2022-04-27 16:08:11 -0300

    refactor(policies): permissões com uma única query
    As permissões são buscadas no banco de dados em uma única query e não
    mais individualmente a cada verificação de permissionamento.

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -36,7 +36,7 @@ abstract class Policy
             key: $user->username . $permission->value,
             seconds: 5,
             callback: function () use ($user, $permission) {
-                return $user->hasPermission($permission);
+                return $this->hasAnyPermission($user, [$permission]);
             }
         );
     }
@@ -62,7 +62,7 @@ abstract class Policy
             key: $user->username . $partial_key,
             seconds: 5,
             callback: function () use ($user, $permissions) {
-                return $user->hasAnyPermission($permissions);
+                return $this->hasAnyPermission($user, $permissions);
             }
         );
     }
@@ -79,5 +79,42 @@ abstract class Policy
     protected function hasPermissionWithoutCache(User $user, PermissionType $permission)
     {
         return (bool) $user->hasPermission($permission);
+    }
+
+    /**
+     * Verfica se o usuário possui uma das permissões informadas.
+     *
+     * @param \App\Models\User $user
+     * @param \App\Enums\PermissionType[] $permissions
+     *
+     * @return bool
+     */
+    protected function hasAnyPermission(User $user, array $permissions)
+    {
+        $match = $this->allPermissionsWithCache($user)->filter(function ($value, $key) use ($permissions) {
+            return in_array(PermissionType::tryFrom($value), $permissions, true);
+        });
+
+        return $match->isNotEmpty();
+    }
+
+    /**
+     * Todas as permissões do usuário.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function allPermissionsWithCache(User $user)
+    {
+        $this->useCache();
+
+        return $this->cache(
+            key: $user->username . '-all-permissions',
+            seconds: 5,
+            callback: function () use ($user) {
+                return $user->permissions();
+            }
+        );
     }
 }
