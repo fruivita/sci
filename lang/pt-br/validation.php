3f7b85ef0888d8384b71bd83b1d744db1300c067
FCNO <fcno.dev@gmail.com>
2022-04-27 00:06:59 -0300

    chore(lang): update traduções

--- lang/pt-br/validation.php
+++ lang/pt-br/validation.php
@@ -108,7 +108,7 @@ return [
     'prohibited_if' => 'The :attribute field is prohibited when :other is :value.',
     'prohibited_unless' => 'The :attribute field is prohibited unless :other is in :values.',
     'prohibits' => 'The :attribute field prohibits :other from being present.',
-    'regex' => 'The :attribute format is invalid.',
+    'regex' => ':attribute em formato inválido.',
     'required' => ':attribute é obrigatório.',
     'required_array_keys' => 'The :attribute field must contain entries for: :values.',
     'required_if' => 'The :attribute field is required when :other is :value.',
