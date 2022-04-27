1ee84f66d3510d91fa98102aa2ba7d1c8ab4d0c3
FCNO <fcno.dev@gmail.com>
2022-04-27 00:48:39 -0300

    chore(validation): add 'file exists' rule

---
+++ app/Rules/FileExists.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * Verifica se o arquivo existe no storage informado.
+ *
+ * @see https://laravel.com/docs/9.x/validation#custom-validation-rules
+ */
+class FileExists implements Rule
+{
+    /**
+     * Nome do storage onde será verificada a existência do arquivo.
+     *
+     * @var string
+     */
+    public $disk;
+
+    /**
+     * @param string $disk nome do storage para verficiar a existência do
+     * arquivo.
+     *
+     * @return void
+     */
+    public function __construct(string $disk)
+    {
+        $this->disk = $disk;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed  $value
+     *
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return Storage::disk($this->disk)->exists($value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return __('File not found');
+    }
+}
---
+++ tests/Feature/Rules/FileExistsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @see https://pestphp.com/docs/
+ */
+
+use App\Rules\FileExists;
+use Illuminate\Support\Facades\Storage;
+
+test('valida se o arquivo existe ou não no storage', function () {
+    $fake_disk = Storage::fake('log-impressao');
+
+    $rule = new FileExists('log-impressao');
+
+    expect($rule->passes('file', 'foo.txt'))->toBeFalse();
+
+    $fake_disk->put('foo.txt', 'content');
+
+    expect($rule->passes('file', 'foo.txt'))->toBeTrue();
+
+    $this->fake_disk = Storage::fake('log-impressao');
+});
