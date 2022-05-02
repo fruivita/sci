f9b16874b8fa17cbeba7a1fa89b849acc45e9010
FCNO <fcno.dev@gmail.com>
2022-05-02 02:02:04 -0300

    chore(view): add componente link card
    Card para ser utilizado na págian inicial como link para os diversos
    pontos da aplicação.

---
+++ resources/views/components/link-card.blade.php
@@ -0,0 +1,33 @@
+{{--
+    Links estilizados como um card para exibição na página home.
+
+    Props:
+    - icon: ícone svg que será exibido
+    - text: texto de descrição/significado do item
+
+    @see https://laravel.com/docs/9.x/blade
+    @see https://tailwindcss.com/
+    @see https://tailwindcss.com/docs/dark-mode
+    @see https://laravel-livewire.com
+    @see https://alpinejs.dev/
+    @see https://icons.getbootstrap.com/
+--}}
+
+
+@props(['icon', 'text'])
+
+
+<div class="bg-primary-300 rounded dark:bg-secondary-600 shadow-lg shadow-secondary-500 dark:shadow-primary-500">
+
+    <a
+        class="flex flex-col items-center space-y-6 p-3"
+        {{ $attributes }}
+    >
+
+        <x-icon name="{{ $icon }}" class="w-16 h-16"/>
+
+        <span class="text-center break-words">{{ $text }}</span>
+
+    </a>
+
+</div>
