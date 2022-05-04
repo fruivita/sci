024c1c2a5328ab27df91fc8a687d87c220a3605c
FCNO <fcno.dev@gmail.com>
2022-05-03 21:06:06 -0300

    chore(database): add migration para a documentação
    A migration espera o nome da rota da aplicação e a url da documentação.

---
+++ database/migrations/2013_02_02_000000_create_docs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/*
+ * @see https://laravel.com/docs/8.x/migrations
+ * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+ */
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('docs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('app_route_name', 255)->unique();
+            $table->string('doc_link', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docs');
+    }
+};
