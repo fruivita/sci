95a311b3b8c4293c00ba607702cbc5115785c33b
FCNO <fcno.dev@gmail.com>
2022-05-14 03:17:38 -0300

    refactor(database): split long methods

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\LazyCollection;
 
 /**
  * @see https://laravel.com/docs/database-testing
@@ -21,34 +22,45 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('roles')->insert([
+        $now = now()->format('Y-m-d H:i:s');
+
+        DB::table('roles')->insert(
+            $this->allPermissions()
+            ->map(function($item) use ($now) {
+                $item['created_at'] = $now;
+                $item['updated_at'] = $now;
+
+                return $item;
+            })
+            ->toArray()
+        );
+    }
+
+    /**
+     * @return \Illuminate\Support\LazyCollection
+     */
+    private function allPermissions()
+    {
+        return LazyCollection::make([
             [
                 'id' => Role::ADMINISTRATOR,
                 'name' => __('Administrator'),
                 'description' => __('Role with access to all application operations.'),
-                'created_at' => now()->format('Y-m-d H:i:s'),
-                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'id' => Role::INSTITUTIONALMANAGER,
                 'name' => __('Institutional manager'),
                 'description' => __("Role with access to all the application's business functions. Does not have access to administration functions."),
-                'created_at' => now()->format('Y-m-d H:i:s'),
-                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'id' => Role::DEPARTMENTMANAGER,
                 'name' => __('Department manager'),
                 'description' => __("Role with access to the application's business functions restricted to the department itself. Does not have access to administration functions."),
-                'created_at' => now()->format('Y-m-d H:i:s'),
-                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
             [
                 'id' => Role::ORDINARY,
                 'name' => __('Ordinary'),
                 'description' => __('Role with access to only minimal functions.'),
-                'created_at' => now()->format('Y-m-d H:i:s'),
-                'updated_at' => now()->format('Y-m-d H:i:s'),
             ],
         ]);
     }
